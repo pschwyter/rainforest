59d0396e429d1ad60fa485785aef0ef03c41b80a
Philippe Schwyter <pschwyter90@gmail.com>
2014-11-07 14:16:21 -0500

    Added pry and awesome_print, also generated products controller and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.create(name: 'RingStix', description: 'Fun outdoor toy', price_in_cents: 1999)
+Product.create(name: 'iPhone 5', description: 'A cool apple device', price_in_cents: 69999)
+Product.create(name: 'Couch', description: 'Great for sitting on', price_in_cents: 34999)
+Product.create(name: 'Samsung S5', description: 'A cool android phone', price_in_cents: 54999)
